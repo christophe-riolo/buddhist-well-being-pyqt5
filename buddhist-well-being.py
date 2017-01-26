463d26ae761e39e97d072a7dccdf9954172d681b
SunyataZero <tord.dellsen@gmail.com>
2017-01-26 20:59:20 +0100

    Added version info which is printed to stdout at program startup

--- buddhist-well-being.py
+++ buddhist-well-being.py
@@ -1,7 +1,10 @@
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5 import Qt
 import bwb_window
+import sqlite3
 
 ######################
 #
@@ -9,9 +12,13 @@ import bwb_window
 #
 ######################
 
+BWB_APPLICATION_VERSION_SG = "private prototype"
+#BWB_APPLICATION_VERSION_IT = 1
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = bwb_window.WellBeingWindow()
+
 
     # System tray
     tray_icon = QSystemTrayIcon(QIcon("icon.png"), app)
@@ -24,5 +31,13 @@ if __name__ == "__main__":
     tray_menu.addAction(tray_quit_action)
     tray_icon.setContextMenu(tray_menu)
     tray_icon.show()
+
+    print("===== Starting Buddhist Well-Being =====")
+    print("Python version: " + str(sys.version))
+    print("SQLite version: " + str(sqlite3.sqlite_version))
+    print("PySQLite (Python module) version: " + str(sqlite3.version))
+    print("Qt version: " + str(QtCore.qVersion()))
+    print("PyQt (Python module) version: " + str(Qt.PYQT_VERSION_STR))
+    print("Buddhist Well-Being application version: " + str(BWB_APPLICATION_VERSION_SG))
 
     sys.exit(app.exec_())
